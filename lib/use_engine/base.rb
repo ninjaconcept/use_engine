02520f54ec286d8ba624a78b11b4711824750908
Stefan Botzenhart <sb@basiszwo.com>
2010-02-10 10:12:54 +0100

    use last vendored gem found to use the latest version;

--- lib/use_engine/base.rb
+++ lib/use_engine/base.rb
@@ -7,7 +7,6 @@ module UseEngine::Base
   module ClassMethods
   
     def use_engine *args
-    
       options = args.extract_options!
     
       engine_name = args.first
@@ -21,12 +20,11 @@ module UseEngine::Base
   
     def local_engine_version engine_name
       @local_engine_versions ||= {}
-      @local_engine_versions[engine_name.to_sym] ||= Dir.glob("vendor/gems/*").select{|dir| dir =~ Regexp.new("#{engine_name}")}.first.split('-').last rescue nil
+      @local_engine_versions[engine_name.to_sym] ||= Dir.glob("vendor/gems/*").select{|dir| dir =~ Regexp.new("#{engine_name}")}.last.split('-').last rescue nil
     end
-  
-   
+    
     def path_for_class
-      case 
+      case
       when self.ancestors.include?(ActiveRecord::Base)
        then
         "models"
@@ -37,24 +35,22 @@ module UseEngine::Base
       end
     end
     
-     #todo: auto find path for class
+    # TODO: auto find path for class
     # def path_for_class
-    #      @path_for_class ||= {}
-    #      @path_for_class[current_file] ||= _path_for_class
-    #    end
+    #   @path_for_class ||= {}
+    #   @path_for_class[current_file] ||= _path_for_class
+    # end
     #    
-    #    def _path_for_class
-    #      class_path = Pathname.new(File.dirname(current_file))
-    #      relative_path = class_path.relative_path_from(Pathname.new("#{RAILS_ROOT}/app")).to_s
-    #      puts "relative_path: #{relative_path}"
-    #      relative_path.split('/').first
-    #    end
+    # def _path_for_class
+    #   class_path = Pathname.new(File.dirname(current_file))
+    #   relative_path = class_path.relative_path_from(Pathname.new("#{RAILS_ROOT}/app")).to_s
+    #   puts "relative_path: #{relative_path}"
+    #   relative_path.split('/').first
+    # end
     #    
-    #    def current_file 
-    #      as_load_path
-    #    end
+    # def current_file 
+    #   as_load_path
+    # end
     
   end
-  
-  
 end
